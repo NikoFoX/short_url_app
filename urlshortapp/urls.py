6ce06155d4cc59b4d1719ed8c49e1c4b833b515f
MrN <nikofoxy@gmail.com>
2019-01-05 21:57:08 +0100

    Added redirection to short_url view after submitting url

--- urlshortapp/urls.py
+++ urlshortapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "urlshortapp"
 urlpatterns = [
     path('', views.index_view, name='index'),
-    path('shorted', views.short_url_display_view, name='shorted'),
+    path('<short_url_url>/', views.short_url_display_view, name='short-url-display'),
 ]
--- urlshortapp/views.py
+++ urlshortapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponseRedirect
 from .models import ShortUrl, Submitter
 from .forms import UrlForm
@@ -14,17 +14,25 @@ import time
 
 # Create your views here.
 def index_view(request):
+    #### CONFIGURABLE!
     len = 6
-    text = ""
+    text = "" # DEBUG
     if request.method == "POST":
+        # checking of the request's method
         form = UrlForm(request.POST)
         if form.is_valid():
+            # if validation of form successful
             short_urls = ShortUrl.objects.all()
             submitted_url = form.cleaned_data.get('url')
-            # CHECK IF submitted_url IS IN short_urls!
             if submitted_url in list(short_urls.values_list('full_url', flat=True)):
-                text = "Url is already in DB!"
+                # if submitted URL is already in DB,
+                # redirect to existing view of short url
+                existing_short_url = short_urls.get(full_url=submitted_url).short_url
+                text = existing_short_url
+                return redirect('urlshortapp:short-url-display', short_url_url=existing_short_url)
             else:
+                # submitted URL not in DB
+                # randomize new short_url string
                 shortened_url_available = "".join(string.ascii_uppercase + string.digits)
                 while True:
                     shortened_url_string = str()
@@ -33,17 +41,19 @@ def index_view(request):
                         shortened_url_string = shortened_url_string + elem
                     if not shortened_url_string in list(short_urls.values_list("short_url", flat=True)):
                         break
-                text = list(ShortUrl.objects.values_list("short_url", flat=True))
+                text = list(short_urls.values_list("short_url", flat=True))
                 submitters = Submitter.objects.all()
                 url_submitter = random.choice(submitters)
+                # create new entry in DB
                 new_short_url = ShortUrl(
                     short_url = shortened_url_string,
                     full_url = submitted_url,
                     submitter = url_submitter
                 )
                 new_short_url.save()
-
-            #return HttpResponseRedirect('shorted', short_url_url)
+                return redirect('urlshortapp:short-url-display', short_url_url=new_short_url.short_url)
+                #return HttpResponseRedirect(reverse('urlshortapp:shorted', args={new_short_url.short_url})
+            #form = UrlForm()
     else:
         form = UrlForm()
         text= ""
